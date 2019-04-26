e5d3b8f13816e7444fe4fd8bfd80dd2b83325dba
pedrodelatorrediaz <48533457+pedrodelatorrediaz@users.noreply.github.com>
2019-04-26 18:23:43 +0200

    Update exercise4.sh

--- scripts/exercise4.sh
+++ scripts/exercise4.sh
@@ -1,4 +1,24 @@
-#!/bin/sh -v
-set -x
 
-echo "Ejercicio 1"
+# echo "Ejercicio 4"
+# ./exercise4.sh aa
+
+# Se obtienen los argumentos
+ARG1=$1
+
+
+		#bucle que recorre la carpeta especificada y que sean jpg
+
+		for i in $ARG1/*.jpg; 
+		do 
+				#recogemos el tamaño de cada archivo `jpg
+				tamanio1=`du $i | awk '{print $1}'`
+
+				#si el tamaño es mayor o igual a 1024 (1mb) se redimensiona
+				if [ $tamanio1 -ge "1024" ]
+				   then
+				    mogrify -resize 720 $i 
+					echo $i
+				fi
+
+		done
+
